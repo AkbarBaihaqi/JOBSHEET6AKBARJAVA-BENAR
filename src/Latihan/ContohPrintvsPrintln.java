6afcd61038446d5170ac1770e1b00b6cd506244b
Lenovo <Lenovo@LAPTOP-3ABJAA55>
2018-08-02 13:52:26 +0700

    Initial Commit Coba IF

---
+++ src/Latihan/ContohPrintvsPrintln.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ContohPrintvsPrintln {
+    public static void main(String[] args){
+    System.out.print("ini teks yang dicetak dengan print() ");
+    System.out.println("ini teks yang dicetak dengan prnitln() ");
+    System.out.print("teks dicetak ddengan print() lagi ");        
+            
+        
+    }
+    
+}
---
+++ src/Latihan/LuasPersegi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Home;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class LuasPersegi {
+    public static void main(String[] args){
+    
+    Double panjang, lebar, luas;
+            
+    panjang = 20.0;
+    lebar = 10.0;
+    
+    luas = panjang * lebar;
+    
+    System.out.print("Luas persegi panajng = ");
+    System.out.println(luas);
+    
+    
+    }    
+}
